e67125fe621561cfeee4560e4937b5a3e1b1e572
MTL | Alex <92187331+divide29@users.noreply.github.com>
2023-03-09 18:36:52 +0100

    arpcore

--- src/extension.ts
+++ src/extension.ts
@@ -8,21 +8,21 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "qbcore-helper" is now active!');
+	console.log('Congratulations, your extension "arpcore-snippets" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('qbcore-helper.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('arpcore-snippets.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from qbcore-helper!');
+		vscode.window.showInformationMessage('Hello World from arpcore-snippets!');
 	});
 
-	let createPlugin = vscode.commands.registerCommand('qbcore-helper.CreatePlugin', (uri: vscode.Uri) => {
+	let createPlugin = vscode.commands.registerCommand('arpcore-snippets.CreatePlugin', (uri: vscode.Uri) => {
 		// Get the name of the plugin.
 		let pluginNameInputBox = vscode.window.createInputBox();
-		pluginNameInputBox.placeholder = "QBCore Plugin Name";
+		pluginNameInputBox.placeholder = "ARPCore Plugin Name";
 		
 		// const pluginName = vscode.window.showInputBox(pluginNameInputBox);
 
@@ -87,7 +87,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// 	console.error(error);
 		// });
 
-		vscode.window.showInformationMessage('Created QBCore Plugin');
+		vscode.window.showInformationMessage('Created ARPCore Plugin');
 	});
 
 
